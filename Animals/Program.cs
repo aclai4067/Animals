3d38226f95f8ed952e4c39e8a0aa71e0578407fa
Ashley Claiborne <languageisaliquid@gmail.com>
2020-03-03 19:41:20 -0600

    Add project files.

---
+++ Animals/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Animals.Reptiles;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var americanAlligator = new Alligator(6000);
+            americanAlligator.isAquatic = true;
+
+            var fictionalAlligator = new Alligator(200);
+            fictionalAlligator.isAquatic = false;
+
+            americanAlligator.Grunt();
+            fictionalAlligator.Grunt();
+
+            americanAlligator.Run(80);
+            fictionalAlligator.Run(500);
+            americanAlligator.Run(5930);
+        }
+    }
+}
---
+++ Animals/Reptiles/Alligator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals.Reptiles
+{
+    internal class Alligator
+    {
+        public bool isAquatic { get; set; }
+        private int _stepGoal;
+        private int _stepCount;
+
+        public Alligator(int stepGoal)
+        {
+            isAquatic = true;
+            _stepGoal = stepGoal;
+        }
+
+        public void Grunt()
+        {
+            if (isAquatic)
+            {
+                Console.WriteLine("GRRRRRR!");
+            }
+            else
+            {
+                Console.WriteLine("hiss");
+            }
+        }
+
+        public void Run(int steps)
+        {
+            _stepCount += steps;
+
+            Console.WriteLine($"You took {steps} steps");
+
+            if (_stepCount >= _stepGoal)
+            {
+                Console.WriteLine($"Congrats!  You met your goal!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
